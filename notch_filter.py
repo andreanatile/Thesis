a96b52601a8d4a0d4e687ec3241cab9d8c87f300
andreanatile <andrea.natile.an@gmail.com>
2023-09-08 19:29:58 +0200

    notch filter gives always zero

--- notch_filter.py
+++ notch_filter.py
@@ -5,5 +5,45 @@ import matplotlib.pyplot as plt
 import pywt
 
 
-def notch_filter(current_segmentFFT,previous_max_energy_index,sampling_rate,overlap_percentage):
-    None
+
+def notch_filter(current_segmentFFT,previous_max_freq_index,pervious_max_amplitude,sampling_rate,overlap_percentage):
+    
+    # Define parameters for the analysis
+    band_size = 10  # Frequency band size in Hz
+
+    window_size = int(band_size*len(current_segmentFFT)/sampling_rate)  # Size of each analysis window
+    step = int(window_size * (1 - overlap_percentage))  # Step size for overlapping windows
+
+    # Initialize lists to store energy in each band
+    energy_in_bands = []
+
+    # Calculate energy for each frequency band using overlapping windows
+    for i in range(step, int(len(current_segmentFFT)/2)- window_size + 1, step):
+        window_data = current_segmentFFT[i:i+window_size]
+            
+        # Calculate the energy in each band
+        energy_band = np.sum(np.abs(window_data)**2)
+        energy_in_bands.append(energy_band)
+        flag=i
+    """ print('i:'+str(flag))
+    print("widow size:"+ str(window_size)) """
+    
+    max_freq_index=np.argmax(energy_in_bands)
+    max_energy=np.max(energy_in_bands)
+
+    if (abs(max_freq_index-previous_max_freq_index)<=1) & (abs(max_energy-pervious_max_amplitude)<=0.005*pervious_max_amplitude):
+        f1=step*max_freq_index
+        f2=f1+band_size
+        print(max_freq_index)
+        w0=(f1+f2)/sampling_rate #Frequency to remove from the signal,0 < w0 < 1, with w0 = 1 corresponding to half of the sampling frequency.
+        #characterizes notch filter -3 dB bandwidth bw relative to its center frequency, Q = w0/bw
+        Q=w0/band_size
+
+        b,a=signal.iirnotch(w0,Q,sampling_rate)
+        return max_freq_index,max_energy,b,a
+    else:
+        return max_freq_index,max_energy,None,None
+        
+
+
+
--- prova3.py
+++ prova3.py
@@ -4,6 +4,8 @@ from scipy import signal
 import matplotlib.pyplot as plt
 from speed_dependency import Speed_Dependency,filtered_signal
 import featrures_extraction as ft
+from notch_filter import notch_filter
+
 
 Sacc=pd.read_csv("data/smalldrive/Accelerometer.csv")
 location=pd.read_csv('data/smalldrive/Location.csv')
@@ -11,33 +13,34 @@ SaccY=Sacc['Acceleration y (m/s^2)']
 
 segments=ft.Segmentation(SaccY,1000,0.66)
 
-FFT_segments=[]
-# Calculate the corresponding frequency values
 sampling_rate = 400
-frequencies = np.fft.fftfreq(1000, d=1/sampling_rate)
-
+flag=0
+FFT_Segments=[]
+filtered_Segments=[]
 for segment in segments:
-    FFT_segments.append(np.fft.fft(segment))
+    FFT_Segments.append(np.fft.fft(segment))
 
-    
-plt.figure(figsize=(10, 6))
+for i in range(0,len(FFT_Segments)):
+    if i==0:
+        previous_max_freq_index=pervious_max_amplitude=0
+        max_freq_index,max_energy,b,a=notch_filter(FFT_Segments[0],previous_max_freq_index,pervious_max_amplitude,sampling_rate,0.66)
+        previous_max_freq_index,pervious_max_amplitude=max_freq_index,max_energy
+        if b is not None:
+            filtered_Segments.append(signal.filtfilt(b,a,segments[i]))
+        else:
+            filtered_Segments.append(segments[i])
+    else:
+        
+        max_freq_index,max_energy,b,a=notch_filter(FFT_Segments[0],previous_max_freq_index,pervious_max_amplitude,sampling_rate,0.66)
+        previous_max_freq_index,pervious_max_amplitude=max_freq_index,max_energy
+        if b is not None:
+            flag+=1
+            filtered_Segments.append(signal.filtfilt(b,a,segments[i]))
+        else:
+            filtered_Segments.append(segments[i])
 
-# Plot the first Fourier Transform
-plt.subplot(2, 1, 1)  # Subplot 1 (2 rows, 1 column, 1st position)
-plt.plot(frequencies, np.abs(FFT_segments[50]))
-plt.title('Fourier Transform of Acceleration y (m/s^2)')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
-plt.xlim(0, sampling_rate / 2)  
 
-# Plot the second Fourier Transform
-plt.subplot(2, 1, 2)  # Subplot 2 (2 rows, 1 column, 2nd position)
-plt.plot(frequencies, np.abs(FFT_segments[55]))
-plt.title('Fourier Transform of Acceleration y (m/s^2)')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
-plt.xlim(0, sampling_rate / 2)  
+print(len(segments))
+print(flag)
 
-plt.tight_layout()
-
-plt.show()
+ 
