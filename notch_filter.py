3f4a1bfab1b050046ad698a092b3ae71e3048d95
andreanatile <andrea.natile.an@gmail.com>
2023-09-25 14:12:43 +0200

    done

--- notch_filter.py
+++ notch_filter.py
@@ -2,16 +2,17 @@ import numpy as np
 from scipy import signal
 from featrures_extraction import Segmentation
 
+
 def notch_filter(current_segmentFFT, previous_max_freq_index, pervious_max_amplitude, sampling_rate, overlap_percentage):
-    print(current_segmentFFT)
+    
     # Define parameters for the analysis
     band_size = 10  # Frequency band size in Hz
-    frequency_resolution=sampling_rate/len(current_segmentFFT) #0.4
+    frequency_resolution = sampling_rate/len(current_segmentFFT)  # 0.4
 
     window_size = int(band_size*len(current_segmentFFT) /
                       sampling_rate)  # Size of each analysis window 25
     # Step size for overlapping windows
-    step = int(window_size * (1 - overlap_percentage))#9
+    step = int(window_size * (1 - overlap_percentage))  # 9
 
     # Initialize lists to store energy in each band
     energy_in_bands = []
@@ -30,33 +31,36 @@ def notch_filter(current_segmentFFT, previous_max_freq_index, pervious_max_ampli
     if (abs(max_freq_index-previous_max_freq_index) <= 1) & (abs(max_energy-pervious_max_amplitude) <= 0.2*pervious_max_amplitude):
         f1 = step*max_freq_index*frequency_resolution
         f2 = f1+band_size
-        #print(max_freq_index)
+        # print(max_freq_index)
         # Frequency to remove from the signal,0 < w0 < 1, with w0 = 1 corresponding to half of the sampling frequency.
         f0 = (f1+f2)/2
-        w0=f0/(0.5*sampling_rate)
-        
+        w0 = f0#/(0.5*sampling_rate)
+        print("\nFrequenza" + str(f0))
+
+        """ print("\nIndice" + str(i)) """
+
         # characterizes notch filter -3 dB bandwidth bw relative to its center frequency, Q = w0/bw
-        Q = w0/band_size
-        
+        Q = w0/20
+
         # Return the numerator(b) and denominator(a) polynomials of the IIR filter
-        b, a = signal.iirnotch(w0, Q, sampling_rate) 
-        
+        b, a = signal.iirnotch(w0, Q, sampling_rate)
+
         return max_freq_index, max_energy, b, a
     else:
         return max_freq_index, max_energy, None, None
 
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 
-def notch_filtering(data,segment_length,sampling_rate, overlap_percentage):
+
+def notch_filtering(data, segment_length, sampling_rate, overlap_percentage):
     # Segmenting the data
-    segments=Segmentation(data,segment_length,overlap_percentage)
+    segments = Segmentation(data, segment_length, overlap_percentage)
 
     # Calculating the FFT trasform for every segment
     FFT_Segments = []
     filtered_Segments = []
     for segment in segments:
         FFT_Segments.append(np.fft.fft(segment))
-
 
     for i in range(0, len(FFT_Segments)):
         # In case it's the first segment, set the previous frequency of the max band and amplitude at 0
@@ -67,14 +71,14 @@ def notch_filtering(data,segment_length,sampling_rate, overlap_percentage):
             max_freq_index, max_energy, b, a = notch_filter(
                 FFT_Segments[i], previous_max_freq_index, prev_max_energy, sampling_rate, 0.66)
             previous_max_freq_index, prev_max_energy = max_freq_index, max_energy
-        
+
         if b is not None:
-            filtered_segment=signal.filtfilt(b,a,segments[i])
+            filtered_segment = signal.filtfilt(b, a, segments[i])
             filtered_Segments.append(filtered_segment)
             print(i)
         else:
             filtered_Segments.append(segments[i])
-    
+
         """ # Print debugging information
         print("Segment:", i)
         print("Original Segment:", segments[i])
